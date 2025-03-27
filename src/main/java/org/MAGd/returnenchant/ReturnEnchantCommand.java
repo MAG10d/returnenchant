ac637b008696aa493ea070af6c1b870a992fda4a
MAGd_ <lgls.magd@gmail.com>
2025-03-27 09:50:17 +0800

    refactor: remove unused ExcellentEnchants API integration

--- src/main/java/org/MAGd/returnenchant/ReturnEnchantCommand.java
+++ src/main/java/org/MAGd/returnenchant/ReturnEnchantCommand.java
@@ -10,10 +10,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.EnchantmentStorageMeta;
 import org.bukkit.inventory.meta.ItemMeta;
-import org.bukkit.plugin.Plugin;
 
-import java.lang.reflect.Method;
-import java.util.HashMap;
 import java.util.Map;
 
 public class ReturnEnchantCommand implements CommandExecutor {
@@ -56,32 +53,8 @@ public class ReturnEnchantCommand implements CommandExecutor {
         Map<Enchantment, Integer> vanillaEnchants = meta.getEnchants();
         int totalEnchants = 0;
         
-        // 檢查並處理ExcellentEnchants附魔
-        Map<Object, Integer> excellentEnchants = new HashMap<>();
-        boolean hasExcellentEnchants = false;
-        
-        // 檢查ExcellentEnchants插件是否存在並啟用
-        Plugin excellentPlugin = player.getServer().getPluginManager().getPlugin("ExcellentEnchants");
-        if (excellentPlugin != null && excellentPlugin.isEnabled()) {
-            try {
-                // 使用反射獲取ExcellentEnchants的API方法
-                Class<?> apiClass = Class.forName("su.nightexpress.excellentenchants.api.ExcellentEnchantsAPI");
-                Method getItemEnchantsMethod = apiClass.getMethod("getItemEnchants", ItemStack.class);
-                
-                // 獲取物品上的ExcellentEnchants附魔
-                @SuppressWarnings("unchecked")
-                Map<Object, Integer> customEnchants = (Map<Object, Integer>) getItemEnchantsMethod.invoke(null, item);
-                if (customEnchants != null && !customEnchants.isEmpty()) {
-                    excellentEnchants = customEnchants;
-                    hasExcellentEnchants = true;
-                }
-            } catch (Exception e) {
-                plugin.getLogger().warning("無法獲取ExcellentEnchants附魔: " + e.getMessage());
-            }
-        }
-        
         // 檢查是否有任何附魔
-        if (vanillaEnchants.isEmpty() && !hasExcellentEnchants) {
+        if (vanillaEnchants.isEmpty()) {
             player.sendMessage(ChatColor.RED + "此物品沒有任何附魔！");
             return true;
         }
@@ -100,35 +73,6 @@ public class ReturnEnchantCommand implements CommandExecutor {
             }
             
             meta.removeEnchant(entry.getKey());
-        }
-        
-        // 處理ExcellentEnchants附魔
-        if (hasExcellentEnchants) {
-            try {
-                Class<?> apiClass = Class.forName("su.nightexpress.excellentenchants.api.ExcellentEnchantsAPI");
-                Method addEnchantMethod = apiClass.getMethod("addEnchant", ItemStack.class, Object.class, int.class, boolean.class);
-                Method removeEnchantMethod = apiClass.getMethod("removeEnchant", ItemStack.class, Object.class);
-
-                for (Map.Entry<Object, Integer> entry : excellentEnchants.entrySet()) {
-                    // 創建附魔書
-                    ItemStack book = new ItemStack(Material.ENCHANTED_BOOK);
-                    EnchantmentStorageMeta bookMeta = (EnchantmentStorageMeta) book.getItemMeta();
-                    book.setItemMeta(bookMeta);
-                    
-                    // 添加ExcellentEnchants附魔到書本
-                    addEnchantMethod.invoke(null, book, entry.getKey(), entry.getValue(), true);
-                    
-                    // 給予玩家
-                    giveItemToPlayer(player, book);
-                    
-                    // 從原物品移除附魔
-                    removeEnchantMethod.invoke(null, item, entry.getKey());
-                    
-                    totalEnchants++;
-                }
-            } catch (Exception e) {
-                plugin.getLogger().warning("處理ExcellentEnchants附魔時出錯: " + e.getMessage());
-            }
         }
         
         // 更新物品Meta
